39eff3f9ab13ee75daaf3c1ab05b0d770dcb61f6
ismail cüneyt varilci <cvarilci@gmail.com>
2025-08-30 23:36:49 +0300

    adaboost kısmı

---
+++ siniflandirma_calisma.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("16-diabetes.csv")
+
+df.head()
+
+df.info()
+
+df.describe()
+
+df.isnull().sum()
+
+df.columns
+
+columns_to_check = ["Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI"]
+
+for col in columns_to_check:
+    zero_count = (df[col] == 0).sum()
+    zero_percentage = 100 * zero_count /len(df)
+    print(f"{col}: {zero_count} %{zero_percentage:.2f}")
+
+X = df.drop('Outcome', axis = 1)
+y = df['Outcome']
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=15)
+
+columns_to_fill = ["Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI"]
+
+medians = {}
+for col in columns_to_fill:
+    median_value = X_train[X_train[col] != 0][col].median()
+    medians[col] = median_value
+    X_train[col] = X_train[col].replace(0, median_value)
+
+for col in columns_to_fill:
+    X_test[col] = X_test[col].replace(0, medians[col])
+
+X_train.describe()
+
+X_train.describe()
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn.model_selection import GridSearchCV
+
+adaboost_param = {
+        "n_estimators" : [50, 70, 100, 120, 150, 200],
+        "learning_rate" : [0.001, 0.01, 0.1, 1, 10]
+}
+
+grid = GridSearchCV(estimator = AdaBoostClassifier(), param_grid = adaboost_param, cv = 5, verbose = 1, n_jobs = -1)
+
+grid.fit(X_train, y_train)
+
+grid.best_params_
+
+ada = AdaBoostClassifier(learning_rate=1, n_estimators=150)
+
+ada.fit(X_train, y_train)
+y_pred = ada.predict(X_test)
+print(classification_report(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
+
+
+
+
+
